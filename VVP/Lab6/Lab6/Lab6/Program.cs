98e1d709d0b71d3ca57f4e9586c0213ca05f9120
Sheldnet <66388269+Sheldnet@users.noreply.github.com>
2020-10-05 10:42:28 +0300

    Доделал Lab 6 VVp

--- VVP/Lab6/Lab6/Lab6/Program.cs
+++ VVP/Lab6/Lab6/Lab6/Program.cs
@@ -1,4 +1,4 @@
-﻿using System //поключение простраства имён sistem
+﻿using System;//поключение простраства имён sistem
 
 namespace Lab6 
 {
@@ -7,11 +7,11 @@ namespace Lab6
         static void Main(string[] args)
         {
             Console.WriteLine("Задание 1");
-            int a = 10, b = 14, k;
+            int a = 10, b = 14, k; // инициализация переменных для обмена
             Console.WriteLine($"A={a}, B={b}");
-            k = a;
-            a = b;
-            b = k;
+            k = a;  // обмен значений переменных через k
+            a = b; 
+            b = k; 
             Console.WriteLine($"A={a}, B={b}");
             //-------------------------------------------
             Console.WriteLine("Задание 2");
@@ -23,9 +23,9 @@ namespace Lab6
             Console.Write("C=");
             c = int.Parse(Console.ReadLine());
             k = b;
-            b = a;
-            a = c;
-            c = k;
+            b = a;// значение a переносим в b
+            a = c; //a в c
+            c = k; // с в b
             Console.WriteLine($"A={a}, B={b} С={c}");
             //-------------------------------------------
             Console.WriteLine("Задание 3");
@@ -35,49 +35,49 @@ namespace Lab6
             b = int.Parse(Console.ReadLine());
             Console.Write("C=");
             c = int.Parse(Console.ReadLine());
-            k = c;
-            c = a;
-            a = b;
-            b = k;
+            k = c; 
+            c = a; // значение A переносим в C
+            a = b; // B в A
+            b = k; // С в B
             Console.WriteLine($"A={a}, B={b} С={c}");
             //-------------------------------------------
             Console.WriteLine("Задание 4");
-            int x;
+            int x; // обяъвляем переменные x и y для решения уравнения
             int y;
             Console.Write("Введите X=");
             x = int.Parse(Console.ReadLine());
-            int arg1= Convert.ToInt32(Math.Pow(x, 6));
-            int arg2= Convert.ToInt32(Math.Pow(x, 2));
-            y = 3 * arg1 - 6 * arg2 - 7;
+            int arg1= Convert.ToInt32(Math.Pow(x, 6)); //возводим x в степень 6 
+            int arg2= Convert.ToInt32(Math.Pow(x, 2)); //возводим x в степень 3 
+            y = 3 * arg1 - 6 * arg2 - 7; // уравнение 
             Console.WriteLine($"y=3x^6 - 6x^2 - 7={y}");
             //-------------------------------------------
             Console.WriteLine("Задание 5");
             Console.Write("Введите X=");
             x = int.Parse(Console.ReadLine());
-            int z;
+            int z; // объявляем переменную для удобного возведения выражения x-3 в степень
             z = x - 3;
             arg1= Convert.ToInt32(Math.Pow(z, 6));
             arg2= Convert.ToInt32(Math.Pow(x, 3));
-            y = 4 * arg1 - 7 * arg2 + 2;
+            y = 4 * arg1 - 7 * arg2 + 2; //уравнение
             Console.WriteLine($"y=4(x-3)^6 - 7(x-3)^3 + 2={y}");
             //-------------------------------------------
             Console.WriteLine("Задание 6");
             Console.Write("Введите А=");
             a = int.Parse(Console.ReadLine());
-            z = a * a;
-            z = z * z;
-            z = z * z;
+            z = a * a; //вторая степень(первое умножение)
+            z = z * z; //четвёртая степень(второе умножение)
+            z = z * z; //восьмая степень(третье умножение)
             Console.WriteLine($"{a}^8={z}");
             //-------------------------------------------
             Console.WriteLine("Задание 7");
             Console.Write("Введите А=");
             a = int.Parse(Console.ReadLine());
-            int z1;
-            z = a * a;
-            z1 = a * z;
-            z1 = z1 * z;
-            z = z1 * z1;
-            z1 = z * z1;
+            int z1; //новая переменная для нечётной степени
+            z = a * a; //вторая степень (1)
+            z1 = a * z; // третья степень(2)
+            z1 = z1 * z; //пятая степень(3)
+            z = z1 * z1; //десятая степень(4)
+            z1 = z * z1; //пятнадцатая степень(5)
             Console.WriteLine($"{a}^15={z1}");
 
 
