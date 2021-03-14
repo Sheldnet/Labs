dda3a2627cdd982deae8fbac2e3f69bde7141809
Sheldnet <66388269+Sheldnet@users.noreply.github.com>
2021-03-14 12:30:47 +0300

    ЛАБА 3 ООП ИЗИ ПОЛУЧАЕТСЯ

---
+++ CHM/LAB 4/LAB 4/LAB 4.cpp
@@ -0,0 +1,139 @@
+﻿#include <iostream>
+#include <cmath>
+#include <conio.h>
+#define eps 0.0001
+using namespace std;
+
+double f1(double x, double y)
+{
+    return cos(y + 0.5f) + x - 0.8f;
+}
+
+double f2(double x, double y)
+{
+    return sin(x) - 2 * y - 1.6f;
+}
+
+double func11(double x, double y)
+{
+    return 1 ;
+}
+
+double func12(double x, double y)
+{
+    return cos(x);
+}
+
+double func21(double x, double y)
+{   
+    return -sin(y + 0.5);
+}
+
+double func22(double x, double y)
+{
+    return -2;
+}
+
+void ober_matr(double a[2][2])
+{
+    double det, aa;
+    det = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+    aa = a[0][0];
+   // if ((det != 0.0) && (a[0][0] != 0.0))
+    //{
+        a[0][0] = a[1][1] / det;
+        a[1][1] = aa / det;
+        aa = a[0][1];
+        a[0][1] = -a[0][1] / det;
+        a[1][0] = -a[1][0] / det;
+    //}
+   /* else 
+    {
+        cout << "det не должно ровняться 0, a[0;0] не должно ровняться 0";
+    }
+    */
+}
+
+void nuton(double x, double y, double *k)
+{
+    double a[2][2], dx, dy;
+    double x0, y0;
+    do
+    {
+        x0 = x;
+        y0 = y;
+        a[0][0] = func11(x, y);
+        a[0][1] = func12(x, y);
+        a[1][0] = func21(x, y);
+        a[1][1] = func22(x, y);
+        ober_matr(a);
+        dx = -a[0][0] * f1(x, y) + -a[0][1] * f2(x, y);
+        dy = -a[1][0] * f1(x, y) + -a[1][1] * f2(x, y);
+        x = x + dx;
+        y = y + dy;
+        //b[0] = f1(x, y);
+        //b[1] = f2(x, y);
+        //norm = sqrt(b[0] * b[0] + b[1] * b[1]);
+        (*k)++;
+    } while ((abs(x0 - x) >= eps) && (abs(y0 - y) >= eps));
+    cout << x << endl << y << endl;
+}
+    
+double f1x(double x, double y)
+{
+    return 0.8f - cos(y + 0.5f);
+}
+
+double f2y(double x, double y)
+{
+    return (1.6f - sin(x)) / -2.0f;
+}
+
+int main()
+{
+    double x, y;
+    double k = 0.0f;
+    cout << "Метод Ньютона:" << endl;
+    cout << "x = ";
+    cin >> x;
+    cout << "y = ";
+    cin >> y;
+    nuton(x, y, &k);
+    cout << "Количество итераций: " << k << endl;
+    k = 0;
+
+    cout << "Метод простых итераций:" << endl;
+    double x0; cout << "x = "; cin >> x0;
+    double y0; cout << "y = "; cin >> y0;
+    x = 0, y = 0;
+    int i = 0;
+    x = f1x(x0, y0);
+    y = f2y(x0, y0);
+    do
+    {
+        i++;
+        x0 = x;
+        y0 = y;
+        system("cls");
+        cout << "Итерация № " << i << endl;
+        cout << "x " <<(x = f1x(x, y)) << endl;
+        cout << "y " <<(y = f2y(x, y)) << endl;
+    } while ((eps < abs(x0 - x)) && (eps < abs(y0 - y)));
+}
+    /*
+    cout << endl;
+    cout << "Метод Простых итераций:" << endl;
+    double x0 = 0.0f, y0 = 0.0f;
+    do
+    {
+        k++;
+        x = 0.8f - cos(y0 + 0.5f);
+        y = (1.6f - sin(x0)) / -2.0f;
+        x0 = x;
+        y0 = y;
+    } while ((abs(x0-x) > eps) && (abs(y0-y) > eps));
+    cout << "x= " << x << " " << "y= " << y << endl;
+    cout << "Количество итераций: " << k << endl;
+
+    cout << endl;*/
+
--- OOP/Lab 3/Lab 3/Lab 3.cpp
+++ OOP/Lab 3/Lab 3/Lab 3.cpp
@@ -1,9 +1,39 @@
 ﻿#include <iostream>
+#include <cstdlib>
 using namespace std;
 
 enum etype { laborer, secretary, manager, accountant, executive, researcher };
 
+struct sterling
+{
+    int funt;
+    int shiling;
+    int penc;
+    double decfunt;
+    int decfuntInt;
+};
 
+struct timee
+{
+    int minutes;
+    int seconds;
+    int hours;
+    int totalseconds;
+};
+
+struct fraction12
+{
+    int numerator;
+    int denominator;
+};
+
+struct fraction
+{
+    int chislit;
+    char znakDrobi;
+    int znamenat;
+    char znak;
+};
 struct Date
 {
     int day;
@@ -40,6 +70,7 @@ void printInformation4(Employee employee)
 
 int main()
 {
+    /*
     Employee sotrudnik1, sotrudnik2, sotrudnik3;
     cout << "Введите номер и размер пособие первого сотрудника: ";
     cin >> sotrudnik1.nomer >> sotrudnik1.posobie;
@@ -71,6 +102,88 @@ int main()
     printInformation4(sotrudnik2);
     printInformation4(sotrudnik3);
     
+    fraction drop1, drop2;
+
+    int rez8 = 0;
+    char znak8;
+
+    cout << "Введите первую дробь: "; cin >> drop1.chislit >> drop1.znakDrobi>> drop1.znamenat;
+    cout << "Введите вторую дробь: "; cin >> drop2.chislit >> drop2.znakDrobi >> drop2.znamenat;
+    cout << "Введите знак: "; cin >> znak8;
+
+    switch (znak8)
+    {
+    case '+': rez8 = (drop1.chislit * drop2.znamenat + drop1.znamenat * drop2.chislit) / (drop1.znamenat * drop2.znamenat); break;
+    case '-': rez8 = (drop1.chislit * drop2.znamenat - drop1.znamenat * drop2.chislit) / (drop1.znamenat * drop2.znamenat); break;
+    case '*': rez8 = (drop1.chislit * drop2.chislit) / (drop1.znamenat * drop2.znamenat); break;
+    case '/': rez8 = (drop1.chislit * drop2.znamenat) / (drop1.znamenat * drop2.chislit); break;
+    }
+
+    cout << "Результат: " << rez8;
+    
+    
+    timee time1;
+    cout << "Введите часы минуты секунды: "; cin >> time1.hours >> time1.minutes >> time1.seconds;
+    cout << "Это время в сенудах будет: "; 
+    time1.totalseconds = time1.hours * 3600 + time1.minutes * 60 + time1.seconds;
+    cout << time1.totalseconds;
+
+    sterling zad10;
+
+    char znak10;
+    cout << "Десятичных фунтов: ";
+    cin >> zad10.funt >> znak10 >> zad10.penc;
+    zad10.decfuntInt= ((zad10.funt * 100) + zad10.penc) * 2.4;
+    cout << "Это равно : ";
+    zad10.funt = zad10.decfuntInt / 240;
+    zad10.shiling =( zad10.decfuntInt % 240 )/ 12;
+    zad10.penc = (zad10.decfuntInt % 240) % 12;
+    cout << zad10.funt << " " << zad10.shiling << " " << zad10.penc;
+        
+
+    timee time111, time112;
+    char znak111, znak112, znak121, znak122;
+    cout << "Введите часы минуты секунды: "; cin >> time111.hours >> znak111 >> time111.minutes >> znak112 >> time111.seconds;
+    cout << "Это время в секудах будет: ";
+    time111.totalseconds = time111.hours * 3600 + time111.minutes * 60 + time111.seconds;
+    cout << time111.totalseconds << endl;
+    cout << "Введите часы минуты секунды: "; cin >> time112.hours >> znak121 >> time112.minutes >> znak122 >> time112.seconds;
+    cout << "Это время в секудах будет: ";
+    time112.totalseconds = time112.hours * 3600 + time112.minutes * 60 + time112.seconds;
+    cout << time112.totalseconds << endl;
+    time111.totalseconds = time111.totalseconds + time112.totalseconds;
+    time111.hours = time111.totalseconds / 3600;
+    time111.minutes = time111.totalseconds % 3600 / 60;
+    time111.seconds = time111.totalseconds % 3600 % 60;
+    cout << time111.hours << ":" << time111.minutes << ":" << time111.seconds;
+    */
+
+
+    fraction12 f1, f2, sumf;
+    char ch = '/', zn;
+    do
+    {
+        cout << "Введите первую дробь" << endl;
+        cin >> f1.numerator >> ch >> f1.denominator;
+        cout << "Введите символ" << endl;
+        cin >> zn;
+        cout << "Введите вторую дробь" << endl;
+        cin >> f2.numerator >> ch >> f2.denominator;
+        switch (zn)
+        {
+        case '+': sumf.numerator = f1.numerator * f2.denominator + f1.denominator * f2.numerator;
+            sumf.denominator = f1.denominator * f2.denominator; break;
+        case '-':  sumf.numerator = f1.numerator * f2.denominator - f1.denominator * f2.numerator;
+            sumf.denominator = f1.denominator * f2.denominator; break;
+        case '*': sumf.numerator = f1.numerator * f2.denominator;
+            sumf.denominator = f1.denominator * f2.denominator; break;
+        case '/': sumf.numerator = f1.numerator * f2.denominator;
+            sumf.denominator = f1.denominator * f2.numerator; break;
+        }
+        cout << sumf.numerator << ch << sumf.denominator << endl;
+        cout << "ещё раз? y или n" << endl;
+        cin >> ch;
+    } while (ch != 'n');
 
     /*
     etype sotr;
